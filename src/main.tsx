6e664a37cce275f7ce8ac051ff29f51320975faf
Dinda Priatni Sapa'ah <nurekokustiarno@ruangguru.com>
2025-10-31 15:11:28 +0700

    remove log di prod

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,12 @@
+if (import.meta.env.PROD) {
+  const noop = () => {};
+  console.log = noop;
+  console.info = noop;
+  console.debug = noop;
+  console.warn = noop;
+  console.error = noop;
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
